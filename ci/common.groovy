3ce380c5e88426e045e96002b06a939cca095c59
Jakub Sokołowski <jakub@status.im>
2018-08-29 06:54:05 -0400

    build parameters are flaky, use separate Jenkinsfile for PRs
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- ci/common.groovy
+++ ci/common.groovy
@@ -2,7 +2,7 @@ def version() {
   return readFile("${env.WORKSPACE}/VERSION").trim()
 }
 
-def buildBranch(name = null, buildType = params.BUILD_TYPE) {
+def buildBranch(name = null, buildType = params.get(BUILD_TYPE)) {
   /* need to drop origin/ to match definitions of child jobs */
   def branchName = env.GIT_BRANCH.replace('origin/', '')
   /* always pass the BRANCH and BUILD_TYPE params with current branch */
